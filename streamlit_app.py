ac086c8ce0ac64ed694c0e12ce5583233b216d08
Yuri0517 <144981008+Yuri0517@users.noreply.github.com>
2023-09-22 00:08:52 +0000

    ｈｈｈ

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,9 +3,9 @@ import random
 
 fortunes = ["恋実る", "恋少し実る", "恋ちょっと厳しそう", "恋諦めたほうがいいかも", "次の相手に行こうね"]
 
-st.title("恋御籤")
-st.write("下のボタンをクリックして、あなたの恋御籤を引いてください。")
+st.title("恋みくじ")
+st.write("下のボタンをクリックして、あなたの大事な恋を占おう！")
 
-if st.button("恋御籤を引く"):
+if st.button("恋みくじを引く"):
     fortune = random.choice(fortunes)
-    st.write(f"あなたの恋御籤は「{fortune}」です。")
+    st.write(f"あなたの恋みくじは「{fortune}」です。")
